84e944a01cf4e5d040a14493751ae877ba085418
Ashwin202 <ashes192000@gmail.com>
2022-07-13 15:08:01 +0530

    URL Shortner finished

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,15 @@
 const { json } = require("body-parser");
 const bodyParser = require("body-parser");
 var express = require("express");
+var http = require("http");
 const app = express();
 var router = express.Router();
-
 var con = require("./database/db");
-
+var server = http.createServer(app);
 var jsonParser = bodyParser.json();
+
+process.env.PORT = process.env.PORT || 3000;
+
 router
   .route("/urlshort")
   .post(jsonParser, (req, res) => {
@@ -14,15 +17,27 @@ router
     console.log(longurl);
     let uniqueID = Math.random()
       .toString(36)
-      .replace(/[^a-z0-9]/gi, "")
-      .substr(2, 10);
+      .replace(/[^a-z]/gi, "")
+      .substr(0, 6);
     con.query(
-      `insert into urltable(longurl,shorturl) values('${longurl}','${uniqueID}')`,
-      (err) => {
-        if (!err) {
-          res.status(200).json({ status: "Insertion to the database success" });
+      `insert into urltable(longurl,shorturl) values('${longurl}','${uniqueID}')`
+      // (err) => {
+      //   if (!err) {
+      //     res.status(200).json({ status: "Insertion to the database success" });
+      //   } else {
+      //     res.status(200).json({ status: "Insertion of longurl failed" });
+      //   }
+      // }
+    );
+    con.query(
+      `select id,longurl,(concat("localhost:3000/",shorturl)) as shorturl from urltable where shorturl="${uniqueID}";`,
+      (error_urlshort, result_urlshort) => {
+        if (!error_urlshort) {
+          res.json(result_urlshort[0]);
         } else {
-          res.status(200).json({ status: "Insertion of longurl failed" });
+          res.status(500).json({
+            status: "Error in getting the data of the current short URL",
+          });
         }
       }
     );
@@ -32,7 +47,7 @@ router
   });
 
 // redirection to the next page using short url
-router.get("/geturl/:geturl", (req, res) => {
+router.get("/:geturl", (req, res) => {
   var short = req.params.geturl;
   con.query(
     `SELECT longurl FROM urltable WHERE  shorturl="${short}";`,
